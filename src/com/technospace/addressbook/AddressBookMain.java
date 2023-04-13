14f08f4b15d937943f6dfc0f9a6068e5dabae06e
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-04-13 23:27:45 +0530

    [ADDED]:[Janhavi] Address Book Project Setup with Welcome Message

---
+++ src/com/technospace/addressbook/AddressBookMain.java
@@ -0,0 +1,19 @@
+package com.technospace.addressbook;
+
+/***
+ * 
+ * @author Lenovo
+ * AddressBookMain class is used for various operation on AddressBook.
+ */
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/***
+		 * Welcome message for user
+		 */
+		System.out.println("Welcome to Address Book Program");
+	}
+
+}
